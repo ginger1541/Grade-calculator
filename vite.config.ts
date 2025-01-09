1652399aef35ec49ccb9f65b5f87dacc6dc7a959
ginger1541 <dudcksgkgk@naver.com>
2025-01-09 15:47:00 +0900

    vite.config.ts add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/ginger1541/Grade-calculator.git",  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
